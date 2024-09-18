15e6513ce2abcdcf302c9662acf2e8a1bea9261c
Young <yi5oyu@gmail.com>
2024-09-19 04:16:41 +0900

    init: Redis Service 생성
    saveData, getData, deleteData 생성

---
+++ src/main/java/com/example/springboottest/service/RedisService.java
@@ -0,0 +1,23 @@
+package com.example.springboottest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void saveData(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String getData(String key) {
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
+}
