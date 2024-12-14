a90b5f7e7186f38c23f94496ef959c9b7b814262
Young <yi5oyu@gmail.com>
2024-12-14 19:46:32 +0900

    feat : swagger 어노테이션 추가
    user entity, usercontroller 어노테이션 추가

--- src/main/java/com/example/springboottest/controller/UserController.java
+++ src/main/java/com/example/springboottest/controller/UserController.java
@@ -2,6 +2,15 @@ package com.example.springboottest.controller;
 
 import com.example.springboottest.entity.User;
 import com.example.springboottest.service.UserService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +22,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Spring Test API",
+        version = "1.0",
+        description = "Spring boot Test API 문서"
+    )
+)
+@Tag(name = "User", description = "유저 관리")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -31,8 +48,17 @@ public class UserController {
         return userService.getAllUsers();
     }
 
+    @Operation(summary = "ID로 유저 찾기", description = "유니크 ID로 특정 유저 찾음")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "유저 발견",
+            content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "404", description = "유저 없음")
+    })
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id) {
+    public User getUserById(
+        @Parameter(description = "유저의 유니크 ID", required = true, example = "1")
+        @PathVariable Long id
+    ){
         return userService.getUserById(id);
     }
 
--- src/main/java/com/example/springboottest/entity/User.java
+++ src/main/java/com/example/springboottest/entity/User.java
@@ -1,10 +1,19 @@
 package com.example.springboottest.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
+@Schema(description = "User Entity")
 public class User {
+    @Schema(description = "유니크 ID", example = "1")
     private Long id;
+
+    @Schema(description = "이름", example = "lee")
     private String name;
+
+    @Schema(description = "이메일", example = "lee@google.com")
     private String email;
 }
